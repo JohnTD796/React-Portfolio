c6030eb031675144f3774d9e9212cc78cf765063
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-07-18 15:57:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = process.env.PORT || 3001;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: port,
     open: true
   }
 })
